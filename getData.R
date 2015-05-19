411dda2288254fc372eb71844a8fc1fcb92ff08d
waleed <waleed.ayoub@gmail.com>
2015-05-18 22:29:14 -0400

    added comments to outline execution steps downloaded the proper playoff games for 2014/2015 season

--- getData.R
+++ getData.R
@@ -5,13 +5,21 @@ library(dplyr)
 
 all.games <- full.game.database()
 table(all.games$season,all.games$session)
+
 distinct(select(all.games,session))
 distinct(select(all.games,season))
 
+# select only the playoff games for 2014/2015 season
 filter(all.games, session=='Playoffs', season==20142015)
-these.games <- subset(all.games, season == 20142015 & gcode >= 30214 & gcode <=30220)
+playoffgames <- filter(all.games, season == 20142015, session=='Playoffs')
 
-compile.all.games(new.game.table=these.games)
+head(playoffgames)
+
+# download all the game info for playoff games
+compile.all.games(new.game.table=playoffgames)
+
+# figure out how to isolate just the info required for goals/assists
+# then iterate through each file and process
 load("./nhlr-data/20142015-30216-processed.RData")
 game.info$date
 summary(game.info)
@@ -25,3 +33,9 @@ head(goaldata)
 goaldata[1,]
 summary(goaldata)
 goaldata
+
+# create a table with pool participant to player lookup
+
+# merge the 2 tables
+
+# plot the cumulative point haul by day
